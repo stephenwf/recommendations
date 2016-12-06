f2134e68347589d6f5cffac2646f7a1838ac0236
Stephen Fraser <stephenwf@users.noreply.github.com>
2016-12-06 14:45:45 +0000

    ELPP-1859 Added basic hydration from API SDK (#9)

--- src/Recommendations/Process/Hydration.php
+++ src/Recommendations/Process/Hydration.php
@@ -13,21 +13,61 @@ namespace eLife\Recommendations\Process;
 use Assert\Assertion;
 use eLife\ApiSdk\ApiSdk;
 use eLife\Recommendations\RuleModel;
+use LogicException;
+use function GuzzleHttp\Promise\all;
 
 final class Hydration
 {
+    /** @var ApiSdk */
     private $sdk;
+
+    public function getSdk(RuleModel $item)
+    {
+        switch ($item->getType()) {
+            case 'podcast-episode':
+                return $this->sdk->podcastEpisodes();
+                break;
+
+            case 'collection':
+                return $this->sdk->collections();
+                break;
+
+            case 'research-article':
+                return $this->sdk->articles();
+                break;
+
+            default:
+                throw new LogicException('ApiSDK does not exist for that type.');
+        }
+    }
+
+    public function transform(RuleModel $item)
+    {
+        $sdk = $this->getSdk($item);
+        $entity = $sdk->get($item->getId());
+
+        return $entity->wait(true);
+    }
 
     public function __construct(ApiSdk $sdk)
     {
         $this->sdk = $sdk;
     }
 
-    private function hydrateOne(RuleModel $model)
+    private function getModel(RuleModel $model, $unwrap = false)
     {
-        // @todo implementation.
-        // Returns one of our request models.
-        return $model;
+        $sdk = $this->getSdk($model);
+        $entity = $sdk->get($model->getId());
+        if ($unwrap) {
+            return $entity->wait(true);
+        }
+
+        return $entity;
+    }
+
+    public function hydrateOne(RuleModel $model)
+    {
+        return $this->getModel($model, true);
     }
 
     /**
@@ -38,7 +78,8 @@ final class Hydration
     public function hydrateAll(array $rules) : array
     {
         Assertion::allIsInstanceOf(RuleModel::class, $rules);
-        // @todo implementation
-        return array_map([$this, 'hydrateOne'], $rules);
+        $entities = array_map([$this, 'hydrateOne'], $rules);
+
+        return all($entities)->wait(true);
     }
 }
