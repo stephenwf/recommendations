4529f51fce33eacd6fbf3002883ef64d83324203
Stephen Fraser <stephen.fraser@digirati.co.uk>
2017-01-18 14:18:20 +0000

    ELPP-1889 Added optimisation and then disabled it

--- src/Recommendations/Process/Hydration.php
+++ src/Recommendations/Process/Hydration.php
@@ -12,6 +12,8 @@ namespace eLife\Recommendations\Process;
 
 use Assert\Assertion;
 use eLife\ApiSdk\ApiSdk;
+use eLife\ApiSdk\Model\ArticleVersion;
+use eLife\ApiSdk\Model\HasSubjects;
 use eLife\Recommendations\RuleModel;
 use LogicException;
 use function GuzzleHttp\Promise\all;
@@ -20,6 +22,7 @@ final class Hydration
 {
     /** @var ApiSdk */
     private $sdk;
+    private $cache = [];
 
     public function getSdk(RuleModel $item)
     {
@@ -60,18 +63,49 @@ final class Hydration
 
     private function getModel(RuleModel $model, $unwrap = false)
     {
+        if (isset($this->cache[$model->getType()][$model->getId()])) {
+            return $this->cache[$model->getType()][$model->getId()];
+        }
         $sdk = $this->getSdk($model);
         $entity = $sdk->get($model->getId());
         if ($unwrap) {
             return $entity->wait(true);
         }
 
-        return $entity;
+        return $entity->then(function ($model) {
+            if (method_exists($model, 'getType')) {
+                $this->cache[$model->getType()][$model->getId()] = $model;
+                // @todo enable if required.
+                // $this->extractRelatedFrom(new RuleModel($model->getId(), $model->getType()));
+            }
+
+            return $model;
+        });
     }
 
     public function hydrateOne(RuleModel $model)
     {
         return $this->getModel($model);
+    }
+
+    public function extractRelatedFrom(RuleModel $model)
+    {
+        $model = $this->getModel($model, true);
+        if ($model instanceof HasSubjects) {
+            $this->cache['subjects'] = $this->cache['subjects'] ?? [];
+            /** @var $model ArticleVersion */
+            foreach ($model->getSubjects() as $subjected) {
+                $this->cache['subjects'][$subjected->getId()] = $subjected;
+            }
+        }
+
+        if ($model instanceof ArticleVersion) {
+            $this->cache['related-article'] = $this->cache['related-article'] ?? [];
+            /** @var $model ArticleVersion */
+            foreach ($model->getRelatedArticles() as $relatedArticle) {
+                $this->cache[$relatedArticle->getType()][$relatedArticle->getId()] = $relatedArticle;
+            }
+        }
     }
 
     /**
