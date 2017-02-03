e4b1522afa5f1ff1102493d066279b57aea58598
Giorgio Sironi <g.sironi@elifesciences.org>
2017-02-03 14:11:30 +0000

    Added AWS configuration

--- config/end2end.php
+++ config/end2end.php
@@ -2,4 +2,9 @@
 
 return [
     'api_url' => 'http://end2end--gateway.elife.internal',
+    'aws' => [
+        'credential_file' => true,
+        'queue_name' => 'recommendations--end2end',
+        'region' => 'us-east-1',
+    ],
 ];
--- config/prod.php
+++ config/prod.php
@@ -2,4 +2,9 @@
 
 return [
     'api_url' => 'http://prod--gateway.elife.internal',
+    'aws' => [
+        'credential_file' => true,
+        'queue_name' => 'recommendations--prod',
+        'region' => 'us-east-1',
+    ],
 ];
